a2793bf143c3e998dd0a51c9aedb46223de754a5
Peter Jones <pjones@redhat.com>
2008-04-07 12:53:13 -0400

    Fix ia32's callwrap.c

--- efi/ia32/callwrap.c
+++ efi/ia32/callwrap.c
@@ -1,131 +1,131 @@
 
-#define ENTRY(name) \
-  ".globl " #name ";" \
-  ".align 16;" \
-  #name ":"
+#define ENTRY(name)		\
+	".globl " #name ";"	\
+	".align 16;"		\
+	#name ":"
 
-__asm__ __volatile__(
-ENTRY(i386_call0)"
-	pushl	%%ebp
-	movl	%%esp,%%ebp
-	subl	$8, %%esp
-	call	*8(%%ebp)
-	leave
-	ret
-        ");
+asm(
+ENTRY(i386_call0)
+	"pushl	%ebp		\n"
+	"movl	%esp,%ebp	\n"
+	"subl	$8, %esp	\n"
+	"call	*8(%ebp)	\n"
+	"leave			\n"
+	"ret			\n"
+	);
 
-__asm__ __volatile__(
-ENTRY(i386_call1)"
-	pushl	%%ebp
-	movl	%%esp,%%ebp
-	subl	$20, %%esp
-	pushl	12(%%ebp)
-	call	*8(%%ebp)
-	leave
-	ret
-        ");
+asm(
+ENTRY(i386_call1)
+	"pushl	%ebp		\n"
+	"movl	%esp,%ebp	\n"
+	"subl	$20, %esp	\n"
+	"pushl	12(%ebp)	\n"
+	"call	*8(%ebp)	\n"
+	"leave			\n"
+	"ret			\n"
+	);
 
-__asm__ __volatile__(
-ENTRY(i386_call2)"
-	pushl	%%ebp
-	movl	%%esp,%%ebp
-	subl	$16, %%esp
-	pushl	16(%%ebp)
-	pushl	12(%%ebp)
-	call	*8(%%ebp)
-	leave
-	ret
-        ");
+asm(
+ENTRY(i386_call2)
+	"pushl	%ebp		\n"
+	"movl	%esp,%ebp	\n"
+	"subl	$16, %esp	\n"
+	"pushl	16(%ebp)	\n"
+	"pushl	12(%ebp)	\n"
+	"call	*8(%ebp)	\n"
+	"leave			\n"
+	"ret			\n"
+	);
 
-__asm__ __volatile__(
-ENTRY(i386_call3)"
-	pushl	%%ebp
-	movl	%%esp,%%ebp
-	subl	$12, %%esp
-	pushl	20(%%ebp)
-	pushl	16(%%ebp)
-	pushl	12(%%ebp)
-	call	*8(%%ebp)
-	leave
-	ret
-        ");
+asm(
+ENTRY(i386_call3)
+	"pushl	%ebp		\n"
+	"movl	%esp,%ebp	\n"
+	"subl	$12, %esp	\n"
+	"pushl	20(%ebp)	\n"
+	"pushl	16(%ebp)	\n"
+	"pushl	12(%ebp)	\n"
+	"call	*8(%ebp)	\n"
+	"leave			\n"
+	"ret			\n"
+	);
 
-__asm__ __volatile__(
-ENTRY(i386_call4)"
-	pushl	%%ebp
-	movl	%%esp,%%ebp
-	subl	$8, %%esp
-	pushl	24(%%ebp)
-	pushl	20(%%ebp)
-	pushl	16(%%ebp)
-	pushl	12(%%ebp)
-	call	*8(%%ebp)
-	leave
-	ret
-        ");
+asm(
+ENTRY(i386_call4)
+	"pushl	%ebp		\n"
+	"movl	%esp,%ebp	\n"
+	"subl	$8, %esp	\n"
+	"pushl	24(%ebp)	\n"
+	"pushl	20(%ebp)	\n"
+	"pushl	16(%ebp)	\n"
+	"pushl	12(%ebp)	\n"
+	"call	*8(%ebp)	\n"
+	"leave			\n"
+	"ret			\n"
+	);
 
-__asm__ __volatile__(
-ENTRY(i386_call5)"
-	pushl	%%ebp
-	movl	%%esp,%%ebp
-	subl	$20, %%esp
-	pushl	28(%%ebp)
-	pushl	24(%%ebp)
-	pushl	20(%%ebp)
-	pushl	16(%%ebp)
-	pushl	12(%%ebp)
-	call	*8(%%ebp)
-	leave
-	ret
-        ");
+asm(
+ENTRY(i386_call5)
+	"pushl	%ebp		\n"
+	"movl	%esp,%ebp	\n"
+	"subl	$20, %esp	\n"
+	"pushl	28(%ebp)	\n"
+	"pushl	24(%ebp)	\n"
+	"pushl	20(%ebp)	\n"
+	"pushl	16(%ebp)	\n"
+	"pushl	12(%ebp)	\n"
+	"call	*8(%ebp)	\n"
+	"leave			\n"
+	"ret			\n"
+	);
 
-__asm__ __volatile__(
-ENTRY(i386_64_call5)"
-	pushl	%%ebp
-	movl	%%esp,%%ebp
-	subl	$12, %%esp
-	pushl	36(%%ebp)
-	pushl	32(%%ebp)
-	pushl	28(%%ebp)
-	pushl	24(%%ebp)
-	pushl	20(%%ebp)
-	pushl	16(%%ebp)
-	pushl	12(%%ebp)
-	call	*8(%%ebp)
-	leave
-	ret
-        ");
+asm(
+ENTRY(i386_64_call5)
+	"pushl	%ebp		\n"
+	"movl	%esp,%ebp	\n"
+	"subl	$12, %esp	\n"
+	"pushl	36(%ebp)	\n"
+	"pushl	32(%ebp)	\n"
+	"pushl	28(%ebp)	\n"
+	"pushl	24(%ebp)	\n"
+	"pushl	20(%ebp)	\n"
+	"pushl	16(%ebp)	\n"
+	"pushl	12(%ebp)	\n"
+	"call	*8(%ebp)	\n"
+	"leave			\n"
+	"ret			\n"
+	);
 
 
-__asm__ __volatile__(
-ENTRY(i386_call6)"
-	pushl	%%ebp
-	movl	%%esp,%%ebp
-	subl	$16, %%esp
-	pushl	32(%%ebp)
-	pushl	28(%%ebp)
-	pushl	24(%%ebp)
-	pushl	20(%%ebp)
-	pushl	16(%%ebp)
-	pushl	12(%%ebp)
-	call	*8(%%ebp)
-	leave
-	ret
-        ");
+asm(
+ENTRY(i386_call6)
+	"pushl	%ebp		\n"
+	"movl	%esp,%ebp	\n"
+	"subl	$16, %esp	\n"
+	"pushl	32(%ebp)	\n"
+	"pushl	28(%ebp)	\n"
+	"pushl	24(%ebp)	\n"
+	"pushl	20(%ebp)	\n"
+	"pushl	16(%ebp)	\n"
+	"pushl	12(%ebp)	\n"
+	"call	*8(%ebp)	\n"
+	"leave			\n"
+	"ret			\n"
+	);
 
-__asm__ __volatile__(
-ENTRY(i386_call7)"
-	pushl	%%ebp
-	movl	%%esp,%%ebp
-	subl	$12, %%esp
-	pushl	36(%%ebp)
-	pushl	32(%%ebp)
-	pushl	28(%%ebp)
-	pushl	24(%%ebp)
-	pushl	20(%%ebp)
-	pushl	16(%%ebp)
-	pushl	12(%%ebp)
-	call	*8(%%ebp)
-	leave
-	ret
-        ");
+asm(
+ENTRY(i386_call7)
+	"pushl	%ebp		\n"
+	"movl	%esp,%ebp	\n"
+	"subl	$12, %esp	\n"
+	"pushl	36(%ebp)	\n"
+	"pushl	32(%ebp)	\n"
+	"pushl	28(%ebp)	\n"
+	"pushl	24(%ebp)	\n"
+	"pushl	20(%ebp)	\n"
+	"pushl	16(%ebp)	\n"
+	"pushl	12(%ebp)	\n"
+	"call	*8(%ebp)	\n"
+	"leave			\n"
+	"ret			\n"
+	);
